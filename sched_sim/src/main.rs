f3e959be8c93c7bd2c79b64a7c8664796304bd05
kobayu <yutaro.kobayashi@tier4.jp>
2023-04-21 11:38:13 +0900

    feat: read dag (yaml)

--- sched_sim/src/main.rs
+++ sched_sim/src/main.rs
@@ -1,3 +1,38 @@
+use serde::Deserialize;
+use std::fs::File;
+use std::io::Read;
+use std::collections::HashMap;
+
+#[derive(Debug, Deserialize)]
+struct DAG {
+    directed: bool,
+    graph: HashMap<String, String>,
+    links: Vec<Edge>, 
+    multigraph: bool,
+    nodes: Vec<Nodes>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Edge {
+    Transfer: u32,
+    communication_time: u32,
+    source: u32,
+    target: u32,
+}
+
+#[derive(Debug, Deserialize)]
+struct Nodes {
+    Weight: u32,
+    execution_time: u32,
+    id: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("src/dag_0.yaml").expect("Unable to open file");
+    let mut content = String::new();
+    file.read_to_string(&mut content).expect("Unable to read file");
+
+    let config: DAG = serde_yaml::from_str(&content).expect("Unable to parse YAML");
+
+    println!("Config: {:?}", config);
 }
