4a3b70ada675aebc5c6ec3ce3d6ca70d2c09312a
kobayu <yutaro.kobayashi@tier4.jp>
2023-04-21 15:58:39 +0900

    feat: add edges in main.rs 58 - 82

--- sched_sim/src/main.rs
+++ sched_sim/src/main.rs
@@ -2,7 +2,9 @@ extern crate yaml_rust;
 extern crate petgraph;
 
 use petgraph::graph::Graph;
-use yaml_rust::{YamlLoader, YamlEmitter};
+use petgraph::prelude::*; // for NodeIndex
+// use yaml_rust::{YamlLoader, YamlEmitter};
+use yaml_rust::{YamlLoader};
 use std::fs;
 use std::collections::HashMap;
 
@@ -21,7 +23,7 @@ fn load_yaml(path: &str) -> Vec<yaml_rust::Yaml> {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path = "src/dag_1.yaml"; // Path to YAML file
+    let path = "src/dag_0.yaml"; // Path to YAML file
     let docs = load_yaml(&path); // Load YAML file
     let doc = &docs[0]; // Get the first YAML object
 
@@ -36,32 +38,57 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         for (key, value) in node.as_hash().unwrap() {
             let key_str = key.as_str().unwrap();
             if key_str != "id" { // add all parameters except id
+                println!("key: {:?}, value: {:?}", key_str, value.as_i64().unwrap() as u32);
                 params.insert(key_str.to_owned(), value.as_i64().unwrap() as u32); // add key and value to the HashMap
             }
         }
         
         // add node to the graph
-        let node_a = graph.add_node(NodeData { 
+        let _node_a = graph.add_node(NodeData { 
             id, 
             params, 
         });
-
-        /* 
-        // start of test
-        //print node id and weight 
-        let node_a_data = graph.node_weight(node_a).unwrap(); // get node data, node_a is the node index
-        let id = node_a_data.id;
-        println!("node_a id: {:?}", id);
-        let execution_time = node_a_data.params.get("Weight").unwrap(); // get weight
-        println!("Weight: {:?}", execution_time);
-        // print weight (if weight is not found, print "Weight is not found")
-        match node_a_data.params.get("Weight") {
-            Some(execution_time) => println!("Weight: {:?}", execution_time),
-            None => println!("Weight is not found"),
-        }
-        // end of test
-        */
     }
+
+    //test code
+    for node in graph.node_indices() {
+        println!("Node {}: {:?} : {:?}", graph[node].id, node, graph[node].params);
+    }
+
+    // add edges to the graph
+    for link in doc["links"].as_vec().unwrap() {
+        let source = link["source"].as_i64().unwrap() as u32;
+        let target = link["target"].as_i64().unwrap() as u32;
+        let mut communication_time = 0; //どちらにせよ0になるため、ここで初期化しておく
+        if let Some(communication_time_value) = link["communication_time"].as_i64() {
+            communication_time = communication_time_value as i32;
+        } 
+        //println!("source: {:?}, target: {:?}, communication_time: {:?}", source, target, communication_time);
+        let mut source_node_index = NodeIndex::end();
+        let mut target_node_index = NodeIndex::end();
+        for node in graph.node_indices() {
+            if graph[node].id == source {
+                source_node_index = node;
+            }
+            if graph[node].id == target {
+                target_node_index = node;
+            }
+            if source_node_index != NodeIndex::end() && target_node_index != NodeIndex::end() {
+                break;
+            }
+        }
+        graph.add_edge(source_node_index, target_node_index, communication_time);
+        
+    }
+
+    //test code
+    for edge in graph.edge_indices() {
+        let source = graph.edge_endpoints(edge).unwrap().0;
+        let target = graph.edge_endpoints(edge).unwrap().1;
+        let weight = graph[edge];
+        println!("Edge from Node {} to Node {} with weight {}", graph[source].id, graph[target].id, weight);
+    }
+    
 
     Ok(())
 } 
