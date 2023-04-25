696edf39e9b66d94abb857993067792ad3c7b188
kobayu <yutaro.kobayashi@tier4.jp>
2023-04-25 15:41:42 +0900

    test: add test of fn load_graph_from_yaml

--- sched_sim/lib/src/lib.rs
+++ sched_sim/lib/src/lib.rs
@@ -1,20 +1,45 @@
+//! library for scheduling simulation.
 extern crate yaml_rust;
 extern crate petgraph;
 
 use petgraph::graph::Graph;
-use petgraph::prelude::*; // for NodeIndex
-// use yaml_rust::{YamlLoader, YamlEmitter};
+use petgraph::prelude::*; 
 use yaml_rust::{YamlLoader};
 use std::fs;
 use std::collections::HashMap;
 
-// custom node data structure for graph nodes (petgraph) 
+/// custom node data structure for graph nodes (petgraph) 
 pub struct NodeData {
+    /// node id
     pub id: u32,
+    /// node parameters
     pub params: HashMap<String, u32>,
 }
 
-// load yaml file and return a vector of Yaml objects 
+/// load yaml file and return a vector of Yaml objects 
+/// 
+/// # Arguments
+/// 
+/// *  `path` - yaml file path
+/// 
+/// # Returns
+/// 
+/// *  `docs` - graph object (vector of Yaml objects)
+/// 
+/// # Example
+/// 
+/// ```
+/// use lib::load_yaml;
+/// 
+/// let chainbase_yaml = load_yaml("../tests/chainbase.yaml");
+/// 
+/// assert_eq!(chainbase_yaml[0]["directed"].as_bool().unwrap(), true);
+/// assert_eq!(chainbase_yaml[0]["multigraph"].as_bool().unwrap(), false);
+/// assert_eq!(chainbase_yaml[0]["nodes"].as_vec().unwrap().len(), 22);
+/// assert_eq!(chainbase_yaml[0]["links"].as_vec().unwrap().len(), 25);
+/// ```
+/// 
+
 pub fn load_yaml(path: &str) -> Vec<yaml_rust::Yaml> {
     let f = fs::read_to_string(path);
     let s = f.unwrap().to_string();
@@ -22,48 +47,76 @@ pub fn load_yaml(path: &str) -> Vec<yaml_rust::Yaml> {
     docs
 }
 
-pub fn load_graph_from_yaml(path: &str) -> Graph::<NodeData, i32> {
-    let path = path; // Path to YAML file
-    let docs = load_yaml(&path); // Load YAML file
-    let doc = &docs[0]; // Get the first YAML object
+/// load yaml file and return a graph object (petgraph)
+/// 
+/// # Arguments
+/// 
+/// *  `path` - yaml file path
+/// 
+/// # Returns
+/// 
+/// *  `graph` - graph object (petgraph)
+/// 
+/// # Example
+/// 
+/// ```
+/// use lib::load_graph_from_yaml;
+/// 
+/// let graph = load_graph_from_yaml("../tests/chainbase.yaml");
+/// let first_node = graph.node_indices().next().unwrap();
+/// let first_edge = graph.edge_indices().next().unwrap();
+/// 
+/// assert_eq!(graph.node_count(), 22);
+/// assert_eq!(graph.edge_count(), 25);
+/// assert_eq!(graph[first_node].params.get("execution_time").unwrap(), &73);
+/// assert_eq!(graph[first_node].id, 0);
+/// assert_eq!(graph[graph.edge_endpoints(first_edge).unwrap().0].id, 0);
+/// assert_eq!(graph[graph.edge_endpoints(first_edge).unwrap().1].id, 1);
+/// assert_eq!(graph[first_edge], 0);
+/// ```
+/// 
 
-    // create a graph object 
+pub fn load_graph_from_yaml(path: &str) -> Graph::<NodeData, i32> {
+    let path = path; 
+    let docs = load_yaml(&path);
+    let doc = &docs[0]; 
+
     let mut graph = Graph::<NodeData, i32>::new();
 
-    // add nodes to the graph
+    // add nodes to graph
     for node in doc["nodes"].as_vec().unwrap() { 
-        let mut params = HashMap::new(); // create a HashMap to store node parameters
-        let id = node["id"].as_i64().unwrap() as u32; // get node id
+        let mut params = HashMap::new(); 
+        let id = node["id"].as_i64().unwrap() as u32; 
     
-        // add node parameters to the HashMap
+        // add node parameters to HashMap
         for (key, value) in node.as_hash().unwrap() {
             let key_str = key.as_str().unwrap();
-            if key_str != "id" { // add all parameters except id
-                //println!("key: {:?}, value: {:?}", key_str, value.as_i64().unwrap() as u32);
-                params.insert(key_str.to_owned(), value.as_i64().unwrap() as u32); // add key and value to the HashMap
+            if key_str != "id" { 
+                params.insert(key_str.to_owned(), value.as_i64().unwrap() as u32); 
             }
         }
         
-        // add node to the graph
         let _node_a = graph.add_node(NodeData { 
             id, 
             params, 
         });
     }
 
-
-
-    // add edges to the graph
+    // add edges to graph
     for link in doc["links"].as_vec().unwrap() {
         let source = link["source"].as_i64().unwrap() as u32;
         let target = link["target"].as_i64().unwrap() as u32;
-        let mut communication_time = 0; //どちらにせよ0になるため、ここで初期化しておく
+        let mut communication_time = 0; 
+
+        // communication time obtained from yaml file is in i64 type
         if let Some(communication_time_value) = link["communication_time"].as_i64() {
             communication_time = communication_time_value as i32;
         } 
         
         let mut source_node_index = NodeIndex::end();
         let mut target_node_index = NodeIndex::end();
+
+        // search for node index in graph
         for node in graph.node_indices() {
             if graph[node].id == source {
                 source_node_index = node;
@@ -80,4 +133,109 @@ pub fn load_graph_from_yaml(path: &str) -> Graph::<NodeData, i32> {
     }
 
     graph
+
 } 
+
+#[cfg(test)] 
+mod tests {
+    use super::*;
+
+    #[test]
+    fn load_dag_yaml() {
+        let chainbase_yaml = load_yaml("../tests/chainbase.yaml");
+        let faninout_yaml = load_yaml("../tests/faninout.yaml");
+        let gnp_yaml = load_yaml("../tests/Gnp.yaml");
+
+        //check chainbase yaml file
+        assert_eq!(chainbase_yaml[0]["directed"].as_bool().unwrap(), true, "directed is expected to be true"); // check directed
+        assert_eq!(chainbase_yaml[0]["multigraph"].as_bool().unwrap(), false, "multigraph is expected to be false"); // check multigraph
+        assert_eq!(chainbase_yaml[0]["nodes"].as_vec().unwrap().len(), 22, "number of nodes is expected to be 22"); // check number of nodes
+        assert_eq!(chainbase_yaml[0]["links"].as_vec().unwrap().len(), 25, "number of edges is expected to be 25"); // check number of edges
+        //cehck faninout yaml file
+        assert_eq!(faninout_yaml[0]["directed"].as_bool().unwrap(), true, "directed is expected to be true"); // check directed
+        assert_eq!(faninout_yaml[0]["multigraph"].as_bool().unwrap(), false, "multigraph is expected to be false"); // check multigraph
+        assert_eq!(faninout_yaml[0]["nodes"].as_vec().unwrap().len(), 20, "number of nodes is expected to be 20"); // check number of nodes
+        assert_eq!(faninout_yaml[0]["links"].as_vec().unwrap().len(), 29, "number of edges is expected to be 29"); // check number of edges
+        //check gnp yaml file
+        assert_eq!(gnp_yaml[0]["directed"].as_bool().unwrap(), true, "directed is expected to be true"); // check directed
+        assert_eq!(gnp_yaml[0]["multigraph"].as_bool().unwrap(), false, "multigraph is expected to be false"); // check multigraph
+        assert_eq!(gnp_yaml[0]["nodes"].as_vec().unwrap().len(), 70, "number of nodes is expected to be 70"); // check number of nodes
+        assert_eq!(gnp_yaml[0]["links"].as_vec().unwrap().len(), 233, "number of edges is expected to be 233"); // check number of edges
+
+    }
+    
+    #[test]
+    fn load_cainbase_graph_from_yaml(){
+        let graph = load_graph_from_yaml("../tests/chainbase.yaml");
+        let first_node = graph.node_indices().next().unwrap();
+        let last_node = graph.node_indices().last().unwrap();
+        let first_edge = graph.edge_indices().next().unwrap();
+        let last_edge = graph.edge_indices().last().unwrap();
+
+        assert_eq!(graph.node_count(), 22, "number of nodes is expected to be 22"); // check number of nodes
+        assert_eq!(graph.edge_count(), 25, "number of edges is expected to be 25"); // check number of edges
+        assert_eq!(graph[first_node].params.get("execution_time").unwrap(), &73, "first node execution time is expected to be 73"); // check first node execution time
+        assert_eq!(graph[last_node].params.get("execution_time").unwrap(), &2, "last node execution time is expected to be 2"); // check last node execution time
+        assert_eq!(graph[first_node].id, 0, "first node id is expected to be 0"); // check first node id
+        assert_eq!(graph[last_node].id, 21, "last node id is expected to be 21"); // check last node id
+        assert_eq!(graph[first_node].params.get("period").unwrap(), &50, "first node period is expected to be 50"); // check first node period
+        assert_eq!(graph[graph.edge_endpoints(first_edge).unwrap().0].id, 0, "first edge source node id is expected to be 0"); // check first edge source node id
+        assert_eq!(graph[graph.edge_endpoints(first_edge).unwrap().1].id, 1, "first edge target node id is expected to be 1"); // check first edge target node id
+        assert_eq!(graph[graph.edge_endpoints(last_edge).unwrap().0].id, 21, "last edge source node id is expected to be 21"); // check last edge source node id
+        assert_eq!(graph[graph.edge_endpoints(last_edge).unwrap().1].id, 18, "last edge target node id is expected to be 18"); // check last edge target node id
+        assert_eq!(graph[first_edge], 0, "first edge weight is expected to be 0"); // check first edge weight
+        assert_eq!(graph[last_edge], 0, "last edge weight is expected to be 0"); // check last edge weight
+    }
+
+    #[test]
+    fn load_faninout_graph_from_yaml(){
+        let graph = load_graph_from_yaml("../tests/faninout.yaml");
+        let first_node = graph.node_indices().next().unwrap();
+        let last_node = graph.node_indices().last().unwrap();
+        let first_edge = graph.edge_indices().next().unwrap();
+        let last_edge = graph.edge_indices().last().unwrap();
+
+        assert_eq!(graph.node_count(), 20, "number of nodes is expected to be 20"); // check number of nodes
+        assert_eq!(graph.edge_count(), 29, "number of edges is expected to be 29"); // check number of edges
+        assert_eq!(graph[first_node].params.get("Weight").unwrap(), &4, "first node weight is expected to be 4"); // check first node weight
+        assert_eq!(graph[last_node].params.get("Weight").unwrap(), &1, "last node weight is expected to be 1"); // check last node weight
+        assert_eq!(graph[first_node].params.get("execution_time").unwrap(), &3, "first node execution time is expected to be 3"); // check first node execution time
+        assert_eq!(graph[last_node].params.get("execution_time").unwrap(), &43, "last node execution time is expected to be 43"); // check last node execution time
+        assert_eq!(graph[first_node].id, 0, "first node id is expected to be 0"); // check first node id
+        assert_eq!(graph[last_node].id, 19, "last node id is expected to be 19"); // check last node id
+        assert_eq!(graph[last_node].params.get("end_to_end_deadline").unwrap(), &402, "last node end to end deadline is expected to be 402"); // check last node end to end deadline
+        assert_eq!(graph[graph.edge_endpoints(first_edge).unwrap().0].id, 0, "first edge source node id is expected to be 0"); // check first edge source node id
+        assert_eq!(graph[graph.edge_endpoints(first_edge).unwrap().1].id, 1, "first edge target node id is expected to be 1"); // check first edge target node id
+        assert_eq!(graph[graph.edge_endpoints(last_edge).unwrap().0].id, 18, "last edge source node id is expected to be 18"); // check last edge source node id
+        assert_eq!(graph[graph.edge_endpoints(last_edge).unwrap().1].id, 19, "last edge target node id is expected to be 19"); // check last edge target node id
+        assert_eq!(graph[first_edge], 11); // check first edge weight
+        assert_eq!(graph[last_edge], 2); // check last edge weight
+    }
+    #[test]
+    fn load_gnp_graph_from_yaml(){
+        let graph = load_graph_from_yaml("../tests/Gnp.yaml");
+        let first_node = graph.node_indices().next().unwrap();
+        let last_node = graph.node_indices().last().unwrap();
+        let first_edge = graph.edge_indices().next().unwrap();
+        let last_edge = graph.edge_indices().last().unwrap();
+
+        assert_eq!(graph.node_count(), 70, "number of nodes is expected to be 70"); // check number of nodes
+        assert_eq!(graph.edge_count(), 233, "number of edges is expected to be 233"); // check number of edges
+        assert_eq!(graph[first_node].id, 0, "first node id is expected to be 0"); // check first node id
+        assert_eq!(graph[last_node].id, 69, "last node id is expected to be 69"); // check last node id
+        assert_eq!(graph[first_node].params.get("Weight").unwrap(), &1, "first node weight is expected to be 1"); // check first node weight
+        assert_eq!(graph[last_node].params.get("Weight").unwrap(), &5, "last node weight is expected to be 5"); // check last node weight
+        assert_eq!(graph[first_node].params.get("execution_time").unwrap(), &34, "first node execution time is expected to be 34"); // check first node execution time
+        assert_eq!(graph[last_node].params.get("execution_time").unwrap(), &1, "last node execution time is expected to be 1"); // check last node execution time
+        assert_eq!(graph[first_node].params.get("offset").unwrap(), &4, "first node offset is expected to be 4"); // check first node offset
+        assert_eq!(graph[last_node].params.get("offset").unwrap(), &5, "last node offset is expected to be 5"); // check last node offset
+        assert_eq!(graph[first_node].params.get("period").unwrap(), &6000, "first node period is expected to be 6000"); // check first node period
+        assert_eq!(graph[last_node].params.get("period").unwrap(), &10, "last node period is expected to be 10"); // check last node period
+        assert_eq!(graph[graph.edge_endpoints(first_edge).unwrap().0].id, 0, "first edge source node id is expected to be 0"); // check first edge source node id
+        assert_eq!(graph[graph.edge_endpoints(first_edge).unwrap().1].id, 2, "first edge target node id is expected to be 2"); // check first edge target node id
+        assert_eq!(graph[graph.edge_endpoints(last_edge).unwrap().0].id, 68, "last edge source node id is expected to be 68"); // check last edge source node id
+        assert_eq!(graph[graph.edge_endpoints(last_edge).unwrap().1].id, 14, "last edge target node id is expected to be 14"); // check last edge target node id
+        assert_eq!(graph[first_edge], 0, "first edge weight is expected to be 0"); // check first edge weight
+        assert_eq!(graph[last_edge], 0, "last edge weight is expected to be 0"); // check last edge weight
+    }
+}
--- sched_sim/src/main.rs
+++ sched_sim/src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use lib::load_graph_from_yaml;
 
-// Parserを継承した構造体はArgの代わりに使用することが可能。
+/// Application description and arguments definition using clap crate 
 #[derive(Parser)]
 #[clap(
     name = "sched_sim",
@@ -9,29 +9,30 @@ use lib::load_graph_from_yaml;
     version = "v1.0.0",
     about = "Application short description."
 )]
-
+ 
+/// Application arguments definition using clap crate 
 struct AppArg {
     #[clap(short = 'd', long = "dag_file_path", required = true)]
     /// DAG file path
     file_name: String, 
 }
 
+/// Application main function 
 fn main() {    
     let arg: AppArg = AppArg::parse();
     let graph = load_graph_from_yaml(&arg.file_name);
     
-
-    //test code
     for node in graph.node_indices() {
         println!("Node {}: {:?} : {:?}", graph[node].id, node, graph[node].params);
     }
 
-    //test code
     for edge in graph.edge_indices() {
         let source = graph.edge_endpoints(edge).unwrap().0;
         let target = graph.edge_endpoints(edge).unwrap().1;
         let weight = graph[edge];
-        println!("Edge from Node {} to Node {} with weight {}", graph[source].id, graph[target].id, weight);
+       println!("Edge from Node {} to Node {} with weight {}", graph[source].id, graph[target].id, weight);
     }
+
+
 
 }
