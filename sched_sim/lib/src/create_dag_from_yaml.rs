378524493e93703fad2419e123980e99bb98daeb
kobayu <yutaro.kobayashi@tier4.jp>
2023-04-28 08:59:13 +0900

    feat: consideration of fp params

--- sched_sim/lib/src/create_dag_from_yaml.rs
+++ sched_sim/lib/src/create_dag_from_yaml.rs
@@ -1,6 +1,7 @@
 //! library for scheduling simulation.
 
 use std::fs;
+use yaml_rust::Yaml;
 use yaml_rust::YamlLoader;
 
 use petgraph::graph::Graph;
@@ -8,15 +9,15 @@ use petgraph::prelude::*;
 use std::collections::HashMap;
 
 fn load_yaml(path: &str) -> Vec<yaml_rust::Yaml> {
-    let f = fs::read_to_string(path);
-    let s = f.unwrap();
-    YamlLoader::load_from_str(&s).unwrap()
+    let file_path = fs::read_to_string(path);
+    let file_string = file_path.unwrap();
+    YamlLoader::load_from_str(&file_string).unwrap()
 }
 
 /// custom node data structure for graph nodes (petgraph)
 pub struct NodeData {
     pub id: u32,
-    pub params: HashMap<String, u32>,
+    pub params: HashMap<String, f32>,
 }
 
 /// load yaml file and return a graph object (petgraph)
@@ -45,12 +46,12 @@ pub struct NodeData {
 /// ```
 ///
 
-pub fn create_dag_from_yaml(path: &str) -> Graph<NodeData, i32> {
+pub fn create_dag_from_yaml(path: &str) -> Graph<NodeData, f32> {
     let path = path;
     let docs = load_yaml(path);
     let doc = &docs[0];
 
-    let mut graph = Graph::<NodeData, i32>::new();
+    let mut graph = Graph::<NodeData, f32>::new();
 
     // add nodes to graph
     for node in doc["nodes"].as_vec().unwrap() {
@@ -61,7 +62,15 @@ pub fn create_dag_from_yaml(path: &str) -> Graph<NodeData, i32> {
         for (key, value) in node.as_hash().unwrap() {
             let key_str = key.as_str().unwrap();
             if key_str != "id" {
-                params.insert(key_str.to_owned(), value.as_i64().unwrap() as u32);
+                match value {
+                    Yaml::Integer(_i) => {
+                        params.insert(key_str.to_owned(), value.as_i64().unwrap() as f32);
+                    }
+                    Yaml::Real(_r) => {
+                        params.insert(key_str.to_owned(), value.as_f64().unwrap() as f32);
+                    }
+                    _ => {}
+                }
             }
         }
 
@@ -72,10 +81,16 @@ pub fn create_dag_from_yaml(path: &str) -> Graph<NodeData, i32> {
     for link in doc["links"].as_vec().unwrap() {
         let source = link["source"].as_i64().unwrap() as u32;
         let target = link["target"].as_i64().unwrap() as u32;
-        let mut communication_time = 0;
+        let mut communication_time = 0.0;
 
-        if let Some(communication_time_value) = link["communication_time"].as_i64() {
-            communication_time = communication_time_value as i32;
+        match &link["communication_time"] {
+            Yaml::Integer(communication_time_value) => {
+                communication_time = *communication_time_value as f32;
+            }
+            Yaml::Real(communication_time_value) => {
+                communication_time = communication_time_value.parse::<f32>().unwrap();
+            }
+            _ => {}
         }
 
         let mut source_node_index = NodeIndex::end();
@@ -94,7 +109,6 @@ pub fn create_dag_from_yaml(path: &str) -> Graph<NodeData, i32> {
         }
         graph.add_edge(source_node_index, target_node_index, communication_time);
     }
-
     graph
 }
 
@@ -117,19 +131,19 @@ mod tests {
         ); // check number of nodes
         assert_eq!(
             graph[first_node].params.get("execution_time").unwrap(),
-            &73,
+            &73.0,
             "first node execution time is expected to be 73"
         ); // check first node execution time
         assert_eq!(
             graph[last_node].params.get("execution_time").unwrap(),
-            &2,
+            &2.0,
             "last node execution time is expected to be 2"
         ); // check last node execution time
         assert_eq!(graph[first_node].id, 0, "first node id is expected to be 0"); // check first node id
         assert_eq!(graph[last_node].id, 21, "last node id is expected to be 21"); // check last node id
         assert_eq!(
             graph[first_node].params.get("period").unwrap(),
-            &50,
+            &50.0,
             "first node period is expected to be 50"
         ); // check first node period
         assert_eq!(
@@ -158,10 +172,13 @@ mod tests {
             "last edge target node id is expected to be 18"
         ); // check last edge target node id
         assert_eq!(
-            graph[first_edge], 0,
+            graph[first_edge], 0.0,
             "first edge weight is expected to be 0"
         ); // check first edge weight
-        assert_eq!(graph[last_edge], 0, "last edge weight is expected to be 0");
+        assert_eq!(
+            graph[last_edge], 0.0,
+            "last edge weight is expected to be 0"
+        );
         // check last edge weight
     }
 
@@ -180,22 +197,22 @@ mod tests {
         ); // check number of nodes
         assert_eq!(
             graph[first_node].params.get("Weight").unwrap(),
-            &4,
+            &4.0,
             "first node weight is expected to be 4"
         ); // check first node weight
         assert_eq!(
             graph[last_node].params.get("Weight").unwrap(),
-            &1,
+            &1.0,
             "last node weight is expected to be 1"
         ); // check last node weight
         assert_eq!(
             graph[first_node].params.get("execution_time").unwrap(),
-            &3,
+            &3.0,
             "first node execution time is expected to be 3"
         ); // check first node execution time
         assert_eq!(
             graph[last_node].params.get("execution_time").unwrap(),
-            &43,
+            &43.0,
             "last node execution time is expected to be 43"
         ); // check last node execution time
         assert_eq!(
@@ -224,10 +241,13 @@ mod tests {
             "last edge target node id is expected to be 19"
         ); // check last edge target node id
         assert_eq!(
-            graph[first_edge], 11,
+            graph[first_edge], 11.0,
             "first edge weight is expected to be 11"
         ); // check first edge weight
-        assert_eq!(graph[last_edge], 2, "last edge weight is expected to be 2");
+        assert_eq!(
+            graph[last_edge], 2.0,
+            "last edge weight is expected to be 2"
+        );
         // check last edge weight
     }
 
@@ -248,42 +268,42 @@ mod tests {
         assert_eq!(graph[last_node].id, 69, "last node id is expected to be 69"); // check last node id
         assert_eq!(
             graph[first_node].params.get("Weight").unwrap(),
-            &1,
+            &1.0,
             "first node weight is expected to be 1"
         ); // check first node weight
         assert_eq!(
             graph[last_node].params.get("Weight").unwrap(),
-            &5,
+            &5.0,
             "last node weight is expected to be 5"
         ); // check last node weight
         assert_eq!(
             graph[first_node].params.get("execution_time").unwrap(),
-            &34,
+            &34.0,
             "first node execution time is expected to be 34"
         ); // check first node execution time
         assert_eq!(
             graph[last_node].params.get("execution_time").unwrap(),
-            &1,
+            &1.0,
             "last node execution time is expected to be 1"
         ); // check last node execution time
         assert_eq!(
             graph[first_node].params.get("offset").unwrap(),
-            &4,
+            &4.0,
             "first node offset is expected to be 4"
         ); // check first node offset
         assert_eq!(
             graph[last_node].params.get("offset").unwrap(),
-            &5,
+            &5.0,
             "last node offset is expected to be 5"
         ); // check last node offset
         assert_eq!(
             graph[first_node].params.get("period").unwrap(),
-            &6000,
+            &6000.0,
             "first node period is expected to be 6000"
         ); // check first node period
         assert_eq!(
             graph[last_node].params.get("period").unwrap(),
-            &10,
+            &10.0,
             "last node period is expected to be 10"
         ); // check last node period
         assert_eq!(
@@ -312,10 +332,82 @@ mod tests {
             "last edge target node id is expected to be 14"
         ); // check last edge target node id
         assert_eq!(
-            graph[first_edge], 0,
+            graph[first_edge], 0.0,
             "first edge weight is expected to be 0"
         ); // check first edge weight
-        assert_eq!(graph[last_edge], 0, "last edge weight is expected to be 0");
+        assert_eq!(
+            graph[last_edge], 0.0,
+            "last edge weight is expected to be 0"
+        );
+        // check last edge weight
+    }
+
+    #[test]
+    fn test_create_dag_from_yaml_normal_f32() {
+        let graph = create_dag_from_yaml("../tests/sample_dags/f32.yaml");
+        let first_node = graph.node_indices().next().unwrap();
+        let last_node = graph.node_indices().last().unwrap();
+        let first_edge = graph.edge_indices().next().unwrap();
+        let last_edge = graph.edge_indices().last().unwrap();
+
+        assert_eq!(
+            graph.node_count(),
+            20,
+            "number of nodes is expected to be 20"
+        ); // check number of nodes
+        assert_eq!(
+            graph[first_node].params.get("Weight").unwrap(),
+            &4.1,
+            "first node weight is expected to be 4"
+        ); // check first node weight
+        assert_eq!(
+            graph[last_node].params.get("Weight").unwrap(),
+            &1.0,
+            "last node weight is expected to be 1"
+        ); // check last node weight
+        assert_eq!(
+            graph[first_node].params.get("execution_time").unwrap(),
+            &3.1,
+            "first node execution time is expected to be 3"
+        ); // check first node execution time
+        assert_eq!(
+            graph[last_node].params.get("execution_time").unwrap(),
+            &43.0,
+            "last node execution time is expected to be 43"
+        ); // check last node execution time
+        assert_eq!(
+            graph.edge_count(),
+            29,
+            "number of edges is expected to be 29"
+        ); // check number of edges
+        assert_eq!(
+            graph[graph.edge_endpoints(first_edge).unwrap().0].id,
+            0,
+            "first edge source node id is expected to be 0"
+        ); // check first edge source node id
+        assert_eq!(
+            graph[graph.edge_endpoints(first_edge).unwrap().1].id,
+            1,
+            "first edge target node id is expected to be 1"
+        ); // check first edge target node id
+        assert_eq!(
+            graph[graph.edge_endpoints(last_edge).unwrap().0].id,
+            18,
+            "last edge source node id is expected to be 18"
+        ); // check last edge source node id
+        assert_eq!(
+            graph[graph.edge_endpoints(last_edge).unwrap().1].id,
+            19,
+            "last edge target node id is expected to be 19"
+        ); // check last edge target node id
+        assert_eq!(
+            graph[first_edge], 11.1,
+            "first edge weight is expected to be 11"
+        ); // check first edge weight
+        assert_eq!(
+            graph[last_edge], 2.0,
+            "last edge weight is expected to be 2"
+        );
         // check last edge weight
     }
 
@@ -327,7 +419,13 @@ mod tests {
 
     #[test]
     #[should_panic]
-    fn test_create_dag_from_yaml_disable_f64() {
-        let _graph = create_dag_from_yaml("../tests/sample_dags/f64.yaml");
+    fn test_create_dag_from_yaml_disable_no_yaml() {
+        let _graph = create_dag_from_yaml("../tests/sample_dags/disable_path.tex");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_create_dag_from_yaml_disable_broken_link() {
+        let _graph = create_dag_from_yaml("../tests/sample_dags/broken_link.yaml");
     }
 }
