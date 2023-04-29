69db77cdc489a8e9a30577937c86999519c7d561
kobayu <yutaro.kobayashi@tier4.jp>
2023-04-29 13:25:15 +0900

    feat: get and push crit path wect

--- sched_sim/sched/src/federated.rs
+++ sched_sim/sched/src/federated.rs
@@ -5,17 +5,30 @@ pub fn federated(folder_path: &str) {
     let dag_set = create_dag_set_from_dir(folder_path);
     let mut deadlines: Vec<f32> = Vec::new();
     let mut sum_wects: Vec<f32> = Vec::new();
+    let mut critical_path_wects: Vec<f32> = Vec::new();
+
     for dag in dag_set {
         let last_node = dag.node_indices().last().unwrap();
-        let deadline = dag[last_node].params["end_to_end_deadline"];
-        deadlines.push(deadline);
-        //全てのノードのexecution_timeの合計を計算
         let mut sum_wect = 0.0;
+        deadlines.push(dag[last_node].params["end_to_end_deadline"]);
+
         for node in dag.node_indices() {
             sum_wect += dag[node].params["execution_time"];
         }
         sum_wects.push(sum_wect);
+
+        let mut critical_paths = get_critical_paths(dag.clone());
+        critical_paths[0].pop();
+        critical_paths[0].remove(0);
+
+        critical_path_wects.push(
+            critical_paths[0]
+                .iter()
+                .map(|node| dag[*node].params["execution_time"])
+                .sum(),
+        );
     }
     println!("deadlines: {:?}", deadlines);
     println!("sum_wects: {:?}", sum_wects);
+    println!("critical_path_wects: {:?}", critical_path_wects);
 }
