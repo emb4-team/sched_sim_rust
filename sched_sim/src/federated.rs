712c869a8b650c83b60d0aa5ee1c744133adf64d
kobayu <yutaro.kobayashi@tier4.jp>
2023-05-11 04:33:25 +0900

    test: add federated all-encompassing test

--- sched_sim/src/federated.rs
+++ sched_sim/src/federated.rs
@@ -94,3 +94,120 @@ pub fn federated(dag_set: Vec<Graph<NodeData, f32>>, core_num: usize) -> bool {
     }
     finalize_task_set_allocation(remaining_cores, low_utilizations, core_num)
 }
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    fn create_node(id: i32, key: &str, value: f32) -> NodeData {
+        let mut params = HashMap::new();
+        params.insert(key.to_string(), value);
+        NodeData { id, params }
+    }
+
+    fn create_high_dag() -> Graph<NodeData, f32> {
+        let mut dag = Graph::<NodeData, f32>::new();
+        let n0 = dag.add_node(create_node(0, "execution_time", 3.0));
+        let n1 = dag.add_node(create_node(1, "execution_time", 6.0));
+        let n2 = dag.add_node(create_node(2, "execution_time", 9.0));
+        let n3 = dag.add_node(create_node(3, "execution_time", 6.0));
+        let n4 = dag.add_node(create_node(4, "execution_time", 4.0));
+        let n5 = dag.add_node(create_node(5, "execution_time", 3.0));
+        let n6 = dag.add_node(create_node(6, "execution_time", 5.0));
+        let mut params = HashMap::new();
+        params.insert("execution_time".to_owned(), 10.0);
+        params.insert("end_to_end_deadline".to_owned(), 35.0);
+        let n7 = dag.add_node(NodeData { id: 7, params });
+        dag.add_edge(n0, n1, 1.0);
+        dag.add_edge(n0, n2, 1.0);
+        dag.add_edge(n1, n3, 1.0);
+        dag.add_edge(n2, n4, 1.0);
+        dag.add_edge(n2, n5, 1.0);
+        dag.add_edge(n3, n6, 1.0);
+        dag.add_edge(n4, n6, 1.0);
+        dag.add_edge(n5, n6, 1.0);
+        dag.add_edge(n6, n7, 1.0);
+        dag
+    }
+    fn create_low_dag() -> Graph<NodeData, f32> {
+        let mut dag = Graph::<NodeData, f32>::new();
+        let n0 = dag.add_node(create_node(0, "execution_time", 3.0));
+        let n1 = dag.add_node(create_node(1, "execution_time", 6.0));
+        let n2 = dag.add_node(create_node(2, "execution_time", 9.0));
+        let n3 = dag.add_node(create_node(3, "execution_time", 6.0));
+        let n4 = dag.add_node(create_node(4, "execution_time", 4.0));
+        let n5 = dag.add_node(create_node(5, "execution_time", 3.0));
+        let n6 = dag.add_node(create_node(6, "execution_time", 5.0));
+        let mut params = HashMap::new();
+        params.insert("execution_time".to_owned(), 10.0);
+        params.insert("end_to_end_deadline".to_owned(), 135.0);
+        let n7 = dag.add_node(NodeData { id: 7, params });
+        dag.add_edge(n0, n1, 1.0);
+        dag.add_edge(n0, n2, 1.0);
+        dag.add_edge(n1, n3, 1.0);
+        dag.add_edge(n2, n4, 1.0);
+        dag.add_edge(n2, n5, 1.0);
+        dag.add_edge(n3, n6, 1.0);
+        dag.add_edge(n4, n6, 1.0);
+        dag.add_edge(n5, n6, 1.0);
+        dag.add_edge(n6, n7, 1.0);
+        dag
+    }
+    fn create_unsuited_dag() -> Graph<NodeData, f32> {
+        let mut dag = Graph::<NodeData, f32>::new();
+        let n0 = dag.add_node(create_node(0, "execution_time", 3.0));
+        let n1 = dag.add_node(create_node(1, "execution_time", 6.0));
+        let mut params = HashMap::new();
+        params.insert("execution_time".to_owned(), 10.0);
+        params.insert("end_to_end_deadline".to_owned(), 10.0);
+        let n2 = dag.add_node(NodeData { id: 2, params });
+        dag.add_edge(n0, n1, 1.0);
+        dag.add_edge(n1, n2, 1.0);
+        dag
+    }
+    #[test]
+    fn test_federated_enough_core() {
+        let dag0 = create_high_dag();
+        let dag1 = create_high_dag();
+        let dag2 = create_low_dag();
+        let dag_set = vec![dag0, dag1, dag2];
+        let core_num = 40;
+        let can_schedule = federated(dag_set, core_num);
+        assert!(can_schedule);
+    }
+    #[test]
+    #[should_panic]
+    fn test_federated_lack_cores_for_high_tasks() {
+        let dag0 = create_high_dag();
+        let dag1 = create_high_dag();
+        let dag2 = create_low_dag();
+        let dag_set = vec![dag0, dag1, dag2];
+        let core_num = 1;
+        let can_schedule = federated(dag_set, core_num);
+        assert!(can_schedule);
+    }
+    #[test]
+    #[should_panic]
+    fn test_federated_lack_cores_for_low_tasks() {
+        let dag0 = create_high_dag();
+        let dag1 = create_low_dag();
+        let dag2 = create_low_dag();
+        let dag_set = vec![dag0, dag1, dag2];
+        let core_num = 5;
+        let can_schedule = federated(dag_set, core_num);
+        assert!(can_schedule);
+    }
+    #[test]
+    #[should_panic]
+    fn test_federated_unsuited_tasks() {
+        let dag0 = create_unsuited_dag();
+        let dag1 = create_unsuited_dag();
+        let dag2 = create_unsuited_dag();
+        let dag_set = vec![dag0, dag1, dag2];
+        let core_num = 5;
+        let can_schedule = federated(dag_set, core_num);
+        assert!(can_schedule);
+    }
+}
