bbedf5dea7b17b792251fb832af85284431a65c6
kobayu <yutaro.kobayashi@tier4.jp>
2023-05-11 03:57:19 +0900

    test: add allocate all-encompassing test

--- lib/src/homogeneous.rs
+++ lib/src/homogeneous.rs
@@ -13,8 +13,8 @@ impl HomogeneousProcessor {
         Self { cores }
     }
 
-    pub fn allocate(&mut self, core_id: usize, node_i: NodeIndex, exec_time: f32) {
-        self.cores[core_id].allocate(node_i, exec_time);
+    pub fn allocate(&mut self, core_id: usize, node_i: NodeIndex, exec_time: f32) -> bool {
+        self.cores[core_id].allocate(node_i, exec_time)
     }
 
     pub fn process(&mut self) {
@@ -37,14 +37,59 @@ mod tests {
         NodeData { id, params }
     }
 
+    fn create_dag() -> Graph<NodeData, f32> {
+        let mut dag = Graph::<NodeData, f32>::new();
+        dag.add_node(create_node(0, "execution_time", 4.0));
+        dag.add_node(create_node(1, "execution_time", 6.0));
+        dag
+    }
+
+    #[test]
+    fn test_homogeneous_processor_new() {
+        let processor = HomogeneousProcessor::new(2);
+        assert_eq!(processor.cores.len(), 2);
+        assert!(processor.cores[0].is_idle);
+        assert!(processor.cores[1].is_idle);
+        assert_eq!(processor.cores[0].processing_node, None);
+        assert_eq!(processor.cores[1].processing_node, None);
+        assert_eq!(processor.cores[0].remain_proc_time, 0.0);
+        assert_eq!(processor.cores[1].remain_proc_time, 0.0);
+    }
+
     #[test]
     fn test_homogeneous_processor_allocate() {
-        let mut dag = Graph::<NodeData, ()>::new();
         let mut processor = HomogeneousProcessor::new(2);
-        let n0 = dag.add_node(create_node(0, "execution_time", 4.0));
-        let n1 = dag.add_node(create_node(1, "execution_time", 7.0));
+        let dag = create_dag();
+        let mut node_indices = dag.node_indices();
+        let n0 = node_indices.next().unwrap();
+        let n1 = node_indices.next().unwrap();
+
+        assert!(processor.allocate(0, n0, dag[n0].params["execution_time"]));
+        assert!(processor.cores[1].is_idle);
+        assert!(processor.allocate(1, n1, dag[n1].params["execution_time"]));
+        assert!(!processor.cores[0].is_idle);
+    }
+
+    #[test]
+    fn test_homogeneous_processor_allocate_same_core() {
+        let mut processor = HomogeneousProcessor::new(2);
+        let dag = create_dag();
+        let mut node_indices = dag.node_indices();
+        let n0 = node_indices.next().unwrap();
+        let n1 = node_indices.next().unwrap();
 
         processor.allocate(0, n0, dag[n0].params["execution_time"]);
-        processor.allocate(1, n1, dag[n1].params["execution_time"]);
+        assert!(!processor.allocate(0, n1, dag[n1].params["execution_time"]));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_homogeneous_processor_allocate_no_exist_core() {
+        let mut processor = HomogeneousProcessor::new(2);
+        let dag = create_dag();
+        let mut node_indices = dag.node_indices();
+        let n0 = node_indices.next().unwrap();
+
+        processor.allocate(2, n0, dag[n0].params["execution_time"]);
     }
 }
