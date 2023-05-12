5cb2772fe29f11cba0760d707ed5401c0899667c
kobayu <yutaro.kobayashi@tier4.jp>
2023-05-12 17:06:50 +0900

    perf: Moving common methods

--- lib/src/core.rs
+++ lib/src/core.rs
@@ -1,7 +1,6 @@
 //! This module contains the definition of the core and the process result enum
 use crate::{core::ProcessResult::*, graph_extension::NodeData};
 use log::warn;
-use petgraph::Graph;
 
 ///enum to represent three types of states
 ///execution not possible because not allocate, execution in progress, execution finished
@@ -64,27 +63,6 @@ impl Core {
     }
 }
 
-pub fn get_minimum_time_unit_from_dag_set(dag_set: &Vec<Graph<NodeData, f32>>) -> f32 {
-    // Initial value is set to 1.0 and returned as is if no decimal point is found.
-    let mut time_unit = 1.0;
-    for dag in dag_set {
-        for node_index in dag.node_indices() {
-            let exec_time = &dag[node_index].params["execution_time"];
-            // Check all nodes in dag_set because we need to use the same unit time in the processor
-            if exec_time.fract() != 0.0 {
-                // -2 to remove "0." from the string
-                let decimal_count =
-                    10u32.pow((exec_time.fract().abs().to_string().chars().count() - 2) as u32);
-                let new_time_unit = 1.0 / decimal_count as f32;
-                if new_time_unit < time_unit {
-                    time_unit = new_time_unit;
-                }
-            }
-        }
-    }
-    time_unit
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -95,6 +73,7 @@ mod tests {
         params.insert(key.to_string(), value);
         NodeData { id, params }
     }
+
     #[test]
     fn test_core_default_params() {
         let core = Core::default();
@@ -150,32 +129,5 @@ mod tests {
         assert!(core.is_idle);
         assert_eq!(core.processing_node, None);
         assert_eq!(core.remain_proc_time, 0.0);
-    }
-
-    #[test]
-    fn test_get_minimum_time_unit_from_dag_set() {
-        let mut dag0 = Graph::<NodeData, f32>::new();
-        dag0.add_node(create_node(0, "execution_time", 30.0));
-
-        let mut dag_set = vec![dag0];
-        assert_eq!(get_minimum_time_unit_from_dag_set(&dag_set), 1.0);
-
-        let mut dag1 = Graph::<NodeData, f32>::new();
-        dag1.add_node(create_node(1, "execution_time", 3.0));
-        dag_set.push(dag1);
-
-        assert_eq!(get_minimum_time_unit_from_dag_set(&dag_set), 1.0);
-
-        let mut dag2 = Graph::<NodeData, f32>::new();
-        dag2.add_node(create_node(2, "execution_time", 0.3));
-        dag_set.push(dag2);
-
-        assert_eq!(get_minimum_time_unit_from_dag_set(&dag_set), 0.1);
-
-        let mut dag3 = Graph::<NodeData, f32>::new();
-        dag3.add_node(create_node(3, "execution_time", 0.03));
-        dag_set.push(dag3);
-
-        assert_eq!(get_minimum_time_unit_from_dag_set(&dag_set), 0.01);
     }
 }
