da2625807ce8317007f2cfc3082cae6a64b92b41
kobayu <yutaro.kobayashi@tier4.jp>
2023-05-16 11:16:12 +0900

    test: Describe dag_set information,

--- lib/src/output.rs
+++ lib/src/output.rs
@@ -1,19 +1,18 @@
 use log::warn;
 use petgraph::Graph;
-use serde_derive::Serialize;
+use serde_derive::{Deserialize, Serialize};
 use serde_yaml;
 use std::fs;
-use std::io::Write;
 
 use crate::graph_extension::{GraphExtension, NodeData};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 struct DAGSet {
     total_utilization: f32,
     dag_set: Vec<DAG>,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 #[allow(clippy::upper_case_acronyms)]
 struct DAG {
     critical_path_length: f32,
@@ -65,8 +64,6 @@ pub fn dag_set_info_to_yaml_file(mut dag_set: Vec<Graph<NodeData, f32>>, file_pa
 
 #[cfg(test)]
 mod tests {
-    use crate::dag_creator::create_dag_from_yaml;
-
     use super::*;
     use std::{collections::HashMap, fs::remove_file};
 
@@ -76,11 +73,38 @@ mod tests {
         NodeData { id, params }
     }
 
+    fn create_dag() -> Graph<NodeData, f32> {
+        let mut dag = Graph::<NodeData, f32>::new();
+        let n0 = {
+            let mut params = HashMap::new();
+            params.insert("execution_time".to_owned(), 4.0);
+            params.insert("period".to_owned(), 10.0);
+            dag.add_node(NodeData { id: 3, params })
+        };
+        let n1 = dag.add_node(create_node(1, "execution_time", 4.0));
+        let n2 = dag.add_node(create_node(2, "execution_time", 3.0));
+        let n3 = dag.add_node(create_node(3, "execution_time", 3.0));
+        dag.add_edge(n0, n1, 1.0);
+        dag.add_edge(n0, n2, 1.0);
+        dag.add_edge(n0, n3, 1.0);
+
+        dag
+    }
+
     #[test]
     fn test() {
-        let mut dag = Graph::<NodeData, f32>::new();
-        let n0 = dag.add_node(create_node(0, "execution_time", 4.0));
-        let n1 = dag.add_node(create_node(1, "execution_time", 7.0));
-        dag.add_edge(n0, n1, 1.0);
+        let dag_set = vec![create_dag(), create_dag()];
+        let file_path = create_yaml_file("../outputs", "test");
+        dag_set_info_to_yaml_file(dag_set, &file_path);
+        let file_contents = std::fs::read_to_string(&file_path).unwrap();
+        let dag_set: DAGSet = serde_yaml::from_str(&file_contents).unwrap();
+
+        assert_eq!(dag_set.total_utilization, 2.8);
+        assert_eq!(dag_set.dag_set.len(), 2);
+        assert_eq!(dag_set.dag_set[1].critical_path_length, 8.0);
+        assert_eq!(dag_set.dag_set[1].end_to_end_deadline, 10.0);
+        assert_eq!(dag_set.dag_set[1].volume, 14.0);
+
+        remove_file(file_path).unwrap();
     }
 }
