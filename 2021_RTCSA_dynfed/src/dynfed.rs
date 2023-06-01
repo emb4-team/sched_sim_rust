e24904a1c90515840b879472f473848c93ebd706
kobayu <yutaro.kobayashi@tier4.jp>
2023-06-01 17:24:32 +0900

    feat: calculate execution order hrn minimum cores

--- 2021_RTCSA_dynfed/src/dynfed.rs
+++ 2021_RTCSA_dynfed/src/dynfed.rs
@@ -5,21 +5,23 @@ use lib::processor::ProcessorBase;
 use petgraph::graph::{Graph, NodeIndex};
 
 #[allow(dead_code)] // TODO: remove
-pub fn pre_computation(dag: &mut Graph<NodeData, i32>) -> Vec<NodeIndex> {
+pub fn calculate_execution_order_minimum_cores(dag: &mut Graph<NodeData, i32>) -> Vec<NodeIndex> {
     let volume = dag.get_volume();
     let end_to_end_deadline = dag.get_end_to_end_deadline().unwrap();
-    let mut m_min = (volume as f32 / end_to_end_deadline as f32).ceil() as usize;
-    let execution_order: Vec<NodeIndex> = Vec::new();
+    let mut min_number_of_cores = (volume as f32 / end_to_end_deadline as f32).ceil() as usize;
 
     #[allow(unused_variables, unused_mut)] // TODO: remove
-    let mut processor = HomogeneousProcessor::new(m_min);
+    let mut processor = HomogeneousProcessor::new(min_number_of_cores);
 
-    while fixed_priority_scheduler(&mut processor, dag) > end_to_end_deadline {
-        m_min += 1;
-        processor = HomogeneousProcessor::new(m_min);
+    let mut result = fixed_priority_scheduler(&mut processor, dag);
+
+    while result.0 > end_to_end_deadline {
+        min_number_of_cores += 1;
+        processor = HomogeneousProcessor::new(min_number_of_cores);
+        result = fixed_priority_scheduler(&mut processor, dag);
     }
 
-    execution_order
+    result.1
 }
 
 #[cfg(test)]
@@ -35,7 +37,7 @@ mod tests {
     }
 
     #[test]
-    fn test_fixed_priority_scheduler_normal() {
+    fn test_calculate_execution_order_minimum_cores_normal() {
         let mut dag = Graph::<NodeData, i32>::new();
         //cX is the Xth critical node.
         let c0 = dag.add_node(create_node(0, "execution_time", 52));
@@ -56,6 +58,6 @@ mod tests {
         dag.add_edge(c0, n0_0, 1);
         dag.add_edge(c0, n1_0, 1);
 
-        pre_computation(&mut dag);
+        calculate_execution_order_minimum_cores(&mut dag);
     }
 }
--- lib/src/fixed_priority_scheduler.rs
+++ lib/src/fixed_priority_scheduler.rs
@@ -46,9 +46,10 @@ const DUMMY_EXECUTION_TIME: i32 = 1;
 pub fn fixed_priority_scheduler(
     processor: &mut impl ProcessorBase,
     dag: &mut Graph<NodeData, i32>,
-) -> i32 {
+) -> (i32, Vec<NodeIndex>) {
     let mut dag = dag.clone();
     let mut current_time = 0;
+    let mut execution_order = Vec::new();
     let mut ready_queue: VecDeque<NodeIndex> = VecDeque::new();
     let mut finish_flag = false;
     let source_node = dag.add_dummy_source_node();
@@ -78,6 +79,7 @@ pub fn fixed_priority_scheduler(
         while let Some(core_index) = processor.get_idle_core_index() {
             if let Some(task) = ready_queue.pop_front() {
                 processor.allocate(core_index, dag[task].clone());
+                execution_order.push(task);
             } else {
                 break;
             }
@@ -135,8 +137,10 @@ pub fn fixed_priority_scheduler(
     dag.remove_dummy_sink_node();
     dag.remove_dummy_source_node();
 
-    //Return the normalized total time taken to finish all tasks.
-    current_time - DUMMY_EXECUTION_TIME * 2
+    execution_order.remove(0); //Remove the dummy source node from the execution order.
+    execution_order.pop(); //Remove the dummy sink node from the execution order.
+                           //Return the normalized total time taken to finish all tasks.
+    (current_time - DUMMY_EXECUTION_TIME * 2, execution_order)
 }
 
 #[cfg(test)]
@@ -180,8 +184,17 @@ mod tests {
 
         let mut homogeneous_processor = HomogeneousProcessor::new(2);
         assert_eq!(
-            fixed_priority_scheduler(&mut homogeneous_processor, &mut dag),
+            fixed_priority_scheduler(&mut homogeneous_processor, &mut dag).0,
             92
+        );
+        assert_eq!(
+            fixed_priority_scheduler(&mut homogeneous_processor, &mut dag).1,
+            vec![
+                NodeIndex::new(0),
+                NodeIndex::new(1),
+                NodeIndex::new(3),
+                NodeIndex::new(2)
+            ]
         );
     }
 
@@ -208,8 +221,18 @@ mod tests {
 
         let mut homogeneous_processor = HomogeneousProcessor::new(3);
         assert_eq!(
-            fixed_priority_scheduler(&mut homogeneous_processor, &mut dag),
+            fixed_priority_scheduler(&mut homogeneous_processor, &mut dag).0,
             92
+        );
+
+        assert_eq!(
+            fixed_priority_scheduler(&mut homogeneous_processor, &mut dag).1,
+            vec![
+                NodeIndex::new(0),
+                NodeIndex::new(1),
+                NodeIndex::new(3),
+                NodeIndex::new(2)
+            ]
         );
     }
 }
