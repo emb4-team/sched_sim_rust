78677b7f47a32732df8b2a42dd8e28f118d5e37e
kobayu <yutaro.kobayashi@tier4.jp>
2023-06-02 21:23:43 +0900

    style: prepare dynfed

--- 2021_RTCSA_dynfed/src/dynfed.rs
+++ 2021_RTCSA_dynfed/src/dynfed.rs
@@ -7,10 +7,10 @@ pub fn dynfed(dag_set: &mut Vec<Graph<NodeData, i32>>) {
     let mut current_time = 0;
 
     let hyper_period = 120; //TODO: get_hyper_period(dag_set);
-    let mut dag_fifo = Vec::new();
+                            //let mut dag_fifo = Vec::new();
     let mut dag_number = Vec::new();
-    //let number_sub_job_cores: HashMap<usize, i32> = HashMap::new();
-    //let number_dag_cores: HashMap<usize, i32> = HashMap::new();
+    let number_sub_job_cores: HashMap<usize, i32> = HashMap::new();
+    let number_dag_cores: HashMap<usize, i32> = HashMap::new();
     let mut is_dag_started: HashMap<usize, bool> = HashMap::new();
 
     for (i, dag) in dag_set.iter().enumerate() {
@@ -20,9 +20,9 @@ pub fn dynfed(dag_set: &mut Vec<Graph<NodeData, i32>>) {
 
     while current_time < hyper_period {
         for dag in dag_number.iter() {
-            if dag.get_head_period().unwrap() == current_time {
-                dag_fifo.push(dag);
-            }
+            //if dag.get_head_offset().unwrap() == current_time {
+            //    dag_fifo.push(dag);
+            //}
         }
         //1つ目
         //2つ目
