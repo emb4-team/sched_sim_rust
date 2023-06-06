b0bcbe58538e6f760be567369115cb8a6fce7355
kobayu <yutaro.kobayashi@tier4.jp>
2023-06-06 16:57:28 +0900

    docs: additional explanation for later

--- 2021_RTCSA_dynfed/src/dynfed.rs
+++ 2021_RTCSA_dynfed/src/dynfed.rs
@@ -1,34 +1,34 @@
+//! dynfed.
+//! Paper Information
+//! -----------------
+//! Title: Timing-Anomaly Free Dynamic Scheduling of Periodic DAG Tasks with Non-Preemptive
+//! Authors: Gaoyang Dai, Morteza Mohaqeqi, and Wang Yi
+//! Conference: RTCSA 2021
+//! -----------------
+
 use lib::{
     graph_extension::{GraphExtension, NodeData},
     util::get_hyper_period,
 };
-use petgraph::Graph;
+use petgraph::{graph::NodeIndex, Graph};
 use std::collections::HashMap;
-
-struct LabeledGraph<NodeData, i32> {
-    label: usize,
-    dag: Graph<NodeData, i32>,
-}
 
 #[allow(dead_code, clippy::ptr_arg)] // TODO: remove
 pub fn dynfed(dag_set: &mut Vec<Graph<NodeData, i32>>) {
     let mut current_time = 0;
-    let mut labeled_dag_set: Vec<LabeledGraph<NodeData, i32>> = Vec::new();
     let hyper_period = get_hyper_period(dag_set);
-    let mut dag_queue: Vec<LabeledGraph<NodeData, i32>> = Vec::new();
+    let mut dag_queue: Vec<Graph<NodeData, i32>> = Vec::new();
     let number_sub_job_cores: HashMap<usize, i32> = HashMap::new();
     let number_dag_cores: HashMap<usize, i32> = HashMap::new();
     let mut is_dag_started: HashMap<usize, bool> = HashMap::new();
 
     for i in 0..dag_set.len() {
         is_dag_started.insert(i, false);
-        let mut labeled_graph = LabeledGraph {
-            label: i,
-            dag: dag_set[i].clone(),
-        };
-        labeled_dag_set.push(labeled_graph);
+        //Managing index of dag with param because Hash cannot be used for key of Hash.
+        dag_set[i].add_param(NodeIndex::new(0), "dag_id", i as i32);
     }
 
+    //Since the evaluation of the original paper is one hyper period.
     while current_time < hyper_period {
         /*for labeled_dag in labeled_dag_set.iter() {
             if labeled_dag.dag.get_head_period() == Some(current_time) {
