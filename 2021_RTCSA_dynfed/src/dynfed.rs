4be1973f17bd483d937e033494208ed748e0a0e6
kobayu <yutaro.kobayashi@tier4.jp>
2023-06-08 14:43:30 +0900

    style: folder name change

--- 2021_RTCSA_dynfed/src/dynfed.rs
+++ 2021_RTCSA_dynfed/src/dynfed.rs
@@ -4,6 +4,7 @@ use lib::homogeneous::HomogeneousProcessor;
 use lib::processor::ProcessorBase;
 use lib::scheduler::SchedulerBase;
 use petgraph::graph::{Graph, NodeIndex};
+use rtss_cpc::prioritization_cpc_model::assign_priority_to_cpc_model;
 
 /// Calculate the execution order when minimum number of cores required to meet the end-to-end deadline.
 ///
@@ -32,6 +33,7 @@ pub fn calculate_minimum_cores_and_execution_order(
     let volume = dag.get_volume();
     let end_to_end_deadline = dag.get_end_to_end_deadline().unwrap();
     let mut minimum_cores = (volume as f32 / end_to_end_deadline as f32).ceil() as usize;
+    assign_priority_to_cpc_model(dag);
     let (mut schedule_length, mut execution_order) =
         FixedPriorityScheduler::schedule(dag, HomogeneousProcessor::new(minimum_cores));
 
@@ -59,23 +61,24 @@ mod tests {
     fn create_sample_dag() -> Graph<NodeData, i32> {
         let mut dag = Graph::<NodeData, i32>::new();
         //cX is the Xth critical node.
-        let c0 = dag.add_node(create_node(0, "execution_time", 52));
-        let c1 = dag.add_node(create_node(1, "execution_time", 40));
-        dag.add_param(c0, "priority", 0);
-        dag.add_param(c1, "priority", 0);
+        let c0 = dag.add_node(create_node(0, "execution_time", 1));
+        let c1 = dag.add_node(create_node(1, "execution_time", 2));
+        let c2 = dag.add_node(create_node(2, "execution_time", 2));
+
         dag.add_param(c0, "end_to_end_deadline", 100);
         //nY_X is the Yth suc node of cX.
-        let n0_0 = dag.add_node(create_node(2, "execution_time", 30));
-        let n1_0 = dag.add_node(create_node(3, "execution_time", 30));
-        dag.add_param(n0_0, "priority", 2);
-        dag.add_param(n1_0, "priority", 1);
+        let n0_0 = dag.add_node(create_node(3, "execution_time", 1));
+        let n1_0 = dag.add_node(create_node(4, "execution_time", 1));
 
         //create critical path edges
         dag.add_edge(c0, c1, 1);
+        dag.add_edge(c1, c2, 1);
 
         //create non-critical path edges
         dag.add_edge(c0, n0_0, 1);
         dag.add_edge(c0, n1_0, 1);
+        dag.add_edge(n0_0, c2, 1);
+        dag.add_edge(n1_0, c2, 1);
 
         dag
     }
@@ -83,16 +86,17 @@ mod tests {
     #[test]
     fn test_calculate_minimum_cores_and_execution_order_normal() {
         let mut dag = create_sample_dag();
-        let (finished_time, execution_order) =
+        let (minimum_cores, execution_order) =
             calculate_minimum_cores_and_execution_order(&mut dag);
 
-        assert_eq!(finished_time, 3);
+        assert_eq!(minimum_cores, 1);
         assert_eq!(
             execution_order,
             vec![
                 NodeIndex::new(0),
                 NodeIndex::new(1),
                 NodeIndex::new(3),
+                NodeIndex::new(4),
                 NodeIndex::new(2)
             ]
         );
