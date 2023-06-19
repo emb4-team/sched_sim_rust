b013b28265e968f9d16bb169605ba9e9853fca02
kobayu <yutaro.kobayashi@tier4.jp>
2023-06-19 20:28:47 +0900

    docs: add help messages for arguments

--- 2020_RTSS_cpc_model_based_algorithm/src/main.rs
+++ 2020_RTSS_cpc_model_based_algorithm/src/main.rs
@@ -11,25 +11,35 @@ use lib::scheduler::DAGSchedulerBase;
 use lib::{dag_creator::*, graph_extension::GraphExtension};
 use outputs_result::dump_cpc_result_to_file;
 
-/// Application description and arguments definition using clap crate
 #[derive(Parser)]
-#[clap()]
-/// Application arguments definition using clap crate
+#[clap(
+    name = "CPC_Model_Based_Algorithm",
+    version = "1.0",
+    about = "About:
+    The CPC_Model_Based_Algorithm operates under the assumption of a constrained deadline.
+    In essence, it presumes that the input Directed Acyclic Graph (DAG) incorporates and adheres to these constrained deadlines.
+    If, however, the input DAG does not contain a predefined constrained deadline, 
+    the algorithm will impose one by multiplying the cycle of the input DAG by an arbitrary multiplier."
+)]
 struct ArgParser {
+    ///Path to DAG file.
     #[clap(short = 'f', long = "dag_file_path", required = true)]
     dag_file_path: String,
+    ///Number of processing cores.
     #[clap(short = 'c', long = "number_of_cores", required = true)]
     number_of_cores: usize,
+    ///Path to output directory.
     #[clap(short = 'o', long = "output_dir_path", default_value = "../outputs")]
     output_dir_path: String,
-    #[clap(short = 'p', long = "period_factor", default_value = "1.0")]
-    period_factor: f32,
+    ///Multiplier to compute constrained deadlines
+    #[clap(short = 'r', long = "ratio_deadline_to_period", default_value = "1.0")]
+    ratio_deadline_to_period: f32,
 }
 
 fn main() {
     let arg: ArgParser = ArgParser::parse();
-    if arg.period_factor > 1.0 {
-        panic!("period_factor must be less than or equal to 1.0");
+    if arg.ratio_deadline_to_period > 1.0 {
+        panic!("ratio_deadline_to_period must be less than or equal to 1.0");
     }
     let mut dag = create_dag_from_yaml(&arg.dag_file_path);
     let homogeneous_processor = HomogeneousProcessor::new(arg.number_of_cores);
@@ -42,12 +52,17 @@ fn main() {
     let constrained_end_to_end_deadline = if let Some(deadline) = dag.get_end_to_end_deadline() {
         deadline as f32
     } else {
-        dag.get_head_period().unwrap() as f32 * arg.period_factor
+        dag.get_head_period().unwrap() as f32 * arg.ratio_deadline_to_period
     };
 
     let result = (schedule_length as f32) < constrained_end_to_end_deadline;
 
-    dump_cpc_result_to_file(&file_path, schedule_length, arg.period_factor, result);
+    dump_cpc_result_to_file(
+        &file_path,
+        schedule_length,
+        arg.ratio_deadline_to_period,
+        result,
+    );
     dump_dag_set_info_to_yaml(&file_path, vec![dag.clone()]);
     dump_processor_info_to_yaml(&file_path, homogeneous_processor);
     dump_processor_log_to_yaml(&file_path, fixed_priority_scheduler.processor_log);
