70ed596d5b9fa83a9785edbfd219ebf12bbedd12
kobayu <yutaro.kobayashi@tier4.jp>
2023-07-07 20:06:25 +0900

    fix: remove duplicates

--- 2014_TPDS_basic_decomposition_based_algorithm/src/handle_segment.rs
+++ 2014_TPDS_basic_decomposition_based_algorithm/src/handle_segment.rs
@@ -19,23 +19,28 @@ pub struct Segment {
 pub fn create_segments(dag: &mut Graph<NodeData, i32>) -> Vec<Segment> {
     dag.calculate_earliest_finish_times();
 
-    let mut node_indices: Vec<_> = dag.node_indices().collect();
-    node_indices
-        .sort_by_key(|&node_i| dag.node_weight(node_i).unwrap().params["earliest_finish_time"]);
+    let mut earliest_finish_times = Vec::new();
 
-    let mut segments: Vec<Segment> = Vec::with_capacity(node_indices.len());
+    for node in dag.node_weights_mut() {
+        earliest_finish_times.push(node.params["earliest_finish_time"]);
+    }
 
-    for i in 0..node_indices.len() {
+    earliest_finish_times.sort();
+    earliest_finish_times.dedup();
+
+    let mut segments: Vec<Segment> = Vec::with_capacity(earliest_finish_times.len());
+
+    for i in 0..earliest_finish_times.len() {
         let begin_range = if i == 0 {
             0
         } else {
-            dag.node_weight(node_indices[i - 1]).unwrap().params["earliest_finish_time"]
+            earliest_finish_times[i - 1]
         };
 
         let segment = Segment {
             nodes: Vec::new(),
             begin_range,
-            end_range: dag.node_weight(node_indices[i]).unwrap().params["earliest_finish_time"],
+            end_range: earliest_finish_times[i],
             deadline: 0.0,
             classification: SegmentClassification::Heavy,
         };
@@ -43,7 +48,7 @@ pub fn create_segments(dag: &mut Graph<NodeData, i32>) -> Vec<Segment> {
         segments.push(segment);
     }
 
-    for &node_i in &node_indices {
+    for node_i in dag.node_indices() {
         let node = dag.node_weight(node_i).unwrap();
 
         for segment in &mut segments {
@@ -85,6 +90,22 @@ mod tests {
         dag
     }
 
+    fn create_duplicates_dag(period: i32) -> Graph<NodeData, i32> {
+        let mut dag = Graph::<NodeData, i32>::new();
+        let n0 = dag.add_node(create_node(0, "execution_time", 4));
+        let n1 = dag.add_node(create_node(1, "execution_time", 7));
+        let n2 = dag.add_node(create_node(2, "execution_time", 7));
+        let n3 = dag.add_node(create_node(3, "execution_time", 36));
+        let n4 = dag.add_node(create_node(4, "execution_time", 54));
+        dag.add_param(n0, "period", period);
+        dag.add_edge(n0, n1, 1);
+        dag.add_edge(n0, n2, 1);
+        dag.add_edge(n1, n3, 1);
+        dag.add_edge(n2, n4, 1);
+
+        dag
+    }
+
     #[test]
     fn test_create_segment_normal() {
         let mut dag = create_sample_dag(120);
@@ -108,5 +129,27 @@ mod tests {
         assert_eq!(segments[3].end_range, 59);
         assert_eq!(segments[4].begin_range, 59);
         assert_eq!(segments[4].end_range, 113);
+    }
+
+    #[test]
+    fn test_create_segment_duplicates() {
+        let mut dag = create_duplicates_dag(120);
+        let segments = create_segments(&mut dag);
+
+        assert_eq!(segments.len(), 4);
+
+        assert_eq!(segments[0].nodes.len(), 1);
+        assert_eq!(segments[1].nodes.len(), 2);
+        assert_eq!(segments[2].nodes.len(), 2);
+        assert_eq!(segments[3].nodes.len(), 1);
+
+        assert_eq!(segments[0].begin_range, 0);
+        assert_eq!(segments[0].end_range, 4);
+        assert_eq!(segments[1].begin_range, 4);
+        assert_eq!(segments[1].end_range, 11);
+        assert_eq!(segments[2].begin_range, 11);
+        assert_eq!(segments[2].end_range, 47);
+        assert_eq!(segments[3].begin_range, 47);
+        assert_eq!(segments[3].end_range, 65);
     }
 }
