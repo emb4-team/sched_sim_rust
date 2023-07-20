6acbc59e9ba642a1fa7d9bcf7d85e64d2c44073c
kobayu <yutaro.kobayashi@tier4.jp>
2023-07-20 10:20:20 +0900

    test: combine with decomposition

--- lib/src/global_earliest_deadline_first_scheduler.rs
+++ lib/src/global_earliest_deadline_first_scheduler.rs
@@ -96,42 +96,45 @@ mod tests {
         NodeData { id, params }
     }
 
+    fn create_sample_dag(period: i32) -> Graph<NodeData, i32> {
+        let mut dag = Graph::<NodeData, i32>::new();
+        let n0 = dag.add_node(create_node(0, "execution_time", 4));
+        let n1 = dag.add_node(create_node(1, "execution_time", 7));
+        let n2 = dag.add_node(create_node(2, "execution_time", 55));
+        let n3 = dag.add_node(create_node(3, "execution_time", 36));
+        let n4 = dag.add_node(create_node(4, "execution_time", 54));
+        dag.add_param(n0, "period", period);
+        dag.add_edge(n0, n1, 1);
+        dag.add_edge(n0, n2, 1);
+        dag.add_edge(n1, n3, 1);
+        dag.add_edge(n2, n4, 1);
+
+        dag
+    }
+
     #[test]
     fn test_global_earliest_deadline_first_scheduler_schedule_normal() {
-        let mut dag = Graph::<NodeData, i32>::new();
-        //cX is the Xth critical node.
-        let c0 = dag.add_node(create_node(0, "execution_time", 52));
-        let c1 = dag.add_node(create_node(1, "execution_time", 40));
-        dag.add_param(c0, "priority", 0);
-        dag.add_param(c0, "period", 100);
-        dag.add_param(c1, "priority", 0);
-        //nY_X is the Yth suc node of cX.
-        let n0_0 = dag.add_node(create_node(2, "execution_time", 12));
-        let n1_0 = dag.add_node(create_node(3, "execution_time", 10));
-        dag.add_param(n0_0, "priority", 2);
-        dag.add_param(n1_0, "priority", 1);
+        let mut dag = create_sample_dag(120);
 
-        //create critical path edges
-        dag.add_edge(c0, c1, 1);
-
-        //create non-critical path edges
-        dag.add_edge(c0, n0_0, 1);
-        dag.add_edge(c0, n1_0, 1);
-
+        dag.add_dummy_source_node();
+        dag.add_dummy_sink_node();
         dag.calculate_latest_finish_times();
+        dag.remove_dummy_source_node();
+        dag.remove_dummy_sink_node();
         let mut global_earliest_deadline_first_scheduler =
             GlobalEarliestDeadlineFirstScheduler::new(&dag, &HomogeneousProcessor::new(2));
         let result = global_earliest_deadline_first_scheduler.schedule();
 
-        assert_eq!(result.0, 102);
+        assert_eq!(result.0, 113);
 
         assert_eq!(
             result.1,
             vec![
                 NodeIndex::new(0),
-                NodeIndex::new(3),
                 NodeIndex::new(2),
-                NodeIndex::new(1)
+                NodeIndex::new(1),
+                NodeIndex::new(3),
+                NodeIndex::new(4)
             ]
         );
     }
