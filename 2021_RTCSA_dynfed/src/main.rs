4df0a8646c938464b571c3a316e72c2517774aed
kobayu <yutaro.kobayashi@tier4.jp>
2023-08-03 16:37:49 +0900

    refactor: code optimization

--- 2021_RTCSA_dynfed/src/main.rs
+++ 2021_RTCSA_dynfed/src/main.rs
@@ -4,6 +4,7 @@ use clap::Parser;
 use dynfed::DynamicFederatedScheduler;
 use lib::dag_creator::*;
 use lib::fixed_priority_scheduler::FixedPriorityScheduler;
+use lib::graph_extension::GraphExtension;
 use lib::homogeneous::HomogeneousProcessor;
 use lib::log::dump_dag_set_scheduler_result_to_yaml;
 use lib::processor::ProcessorBase;
@@ -33,8 +34,8 @@ struct ArgParser {
 
 fn main() {
     let arg: ArgParser = ArgParser::parse();
-    let mut dag_set = create_dag_set_from_dir(&arg.dag_dir_path);
 
+    let mut dag_set = create_dag_set_from_dir(&arg.dag_dir_path);
     adjust_to_implicit_deadline(&mut dag_set);
 
     let homogeneous_processor = HomogeneousProcessor::new(arg.number_of_cores);
@@ -43,24 +44,25 @@ fn main() {
     > = DynamicFederatedScheduler::new(&dag_set, &homogeneous_processor);
 
     dynfed_scheduler.schedule();
-
     let file_path = dynfed_scheduler.dump_log(&arg.output_dir_path, "FixedPriority");
 
-    let yaml_docs = load_yaml(&file_path);
-    let yaml_doc = &yaml_docs[0];
-
-    let mut result = true;
+    // Check the result
+    let yaml_doc = &load_yaml(&file_path)[0];
+    let dag_set_log = &yaml_doc["dag_set_log"];
     let hyper_period = get_hyper_period(&dag_set);
-
-    for dag_id in 0..dag_set.len() {
-        let dag_period = &yaml_doc["dag_set_info"]["each_dag_info"][dag_id]["period"]
-            .as_i64()
-            .unwrap();
-        for release_count in 0..(hyper_period / *dag_period as i32) as usize {
-            let dag_finish_time = &yaml_doc["dag_set_log"][dag_id]["finish_time"][release_count]
+    let mut result = true;
+    'outer: for dag_id in 0..dag_set.len() {
+        let dag_period = dag_set[dag_id].get_head_period().unwrap();
+        let dag_log = &dag_set_log[dag_id]["finish_time"];
+        for release_count in 0..(hyper_period / dag_period) {
+            let dag_finish_time = dag_log[dag_id]["finish_time"][release_count as usize]
                 .as_i64()
                 .unwrap();
-            result = result && *dag_finish_time <= dag_period * (release_count + 1) as i64;
+            result = result && dag_finish_time <= (dag_period * (release_count + 1)) as i64;
+            // If result is false, break the outer loop early using the 'outer label
+            if !result {
+                break 'outer;
+            }
         }
     }
 
